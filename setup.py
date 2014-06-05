b80786154b809dfc5ca13b725c9145e54c0d62f8
Mauricio Souza Lima <mauricio.lima@corp.globo.com>
2014-06-05 10:38:21 -0300

    version 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tornado-alf',
-    version='0.1',
+    version='0.2',
     description="OAuth Client For Tornado",
     long_description=open('README.rst').read(),
     keywords='oauth client client_credentials tornado',
