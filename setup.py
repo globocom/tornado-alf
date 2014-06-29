50a93da16e8d9a53f76ad772f192b10b9de11300
Pablo Aguiar <scorphus@gmail.com>
2014-06-28 21:06:36 -0300

    Release 0.3.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ tests_require = [
 
 setup(
     name='tornado-alf',
-    version='0.3.2',
+    version='0.3.3',
     description="OAuth Client For Tornado",
     long_description=open('README.rst').read(),
     keywords='oauth client client_credentials tornado',
